e211b55a3e806821e1cad8f0bff41da1581055d3
Dan Nelson <dan@luxusgroup.com>
2016-06-17 10:55:57 -0600

    Create the noops file.

---
+++ src/console-noops.js
@@ -0,0 +1,27 @@
+/**
+ * Prevent legacy browsers from shitting themselves when console calls are made.
+ * @see http://stackoverflow.com/a/16916941/545137
+ */
+(function () {
+// Avoid `console` errors in browsers that lack a console.
+  var method;
+  var noop = function () {
+  };
+  var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
+
+  while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
+}());
